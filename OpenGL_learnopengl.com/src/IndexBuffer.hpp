0ac76257a9f41886226e06c51ab77e51c17cf954
Joel Naviliat <yeoa@gmx.de>
2022-03-16 18:32:43 +0100

    add comments to the buffer classes

--- OpenGL_learnopengl.com/src/IndexBuffer.hpp
+++ OpenGL_learnopengl.com/src/IndexBuffer.hpp
@@ -4,15 +4,19 @@
 class IndexBuffer
 {
 private:
+	// the unsigned int thing for OpenGL
 	unsigned int m_RendererID;
+	// number of uints (indeces) in this buffer
 	unsigned int m_Count;
 
 public:
+	// data | sizeof(data)
 	IndexBuffer(const unsigned* data, unsigned int count);
 	~IndexBuffer();
 
 	void bind() const;
 	void unbind() const;
 
+	// get number of uints (indeces) in this buffer
 	inline unsigned int getCount() const { return m_Count; };
 };
--- OpenGL_learnopengl.com/src/VertexBuffer.hpp
+++ OpenGL_learnopengl.com/src/VertexBuffer.hpp
@@ -4,9 +4,11 @@
 class VertexBuffer
 {
 private:
+	// the unsigned int thing for OpenGL
 	unsigned int m_RendererID;
 
 public:
+	// data | sizeof(data)
 	VertexBuffer(const void *data, unsigned int bytes);
 	~VertexBuffer();
 
